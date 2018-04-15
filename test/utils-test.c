2a2b1a2667cd2c215185425e6c6c10708b5331a7
sommd <d.j.s.1050@gmail.com>
2018-04-15 16:00:22 +1000

    Add documentation to tests

--- test/utils-test.c
+++ test/utils-test.c
@@ -39,24 +39,27 @@ void teardown_fsize() {
     fclose(dir_file);
 }
 
+/* Test fsize with an empty file. */
 START_TEST(test_fsize_empty) {
     ck_assert_int_eq(fsize(reg_file), 0);
 } END_TEST
 
+/* Test fsize with variously sized files. */
 START_TEST(test_fsize_sized) {
     size_t size = sizes[_i];
-    unsigned char data[size]; // Doesn't matter what data we write
     
-    fwrite(data, sizeof(*data), size, reg_file);
+    fzero(reg_file, size);
     fflush(reg_file);
     
     ck_assert_int_eq(fsize(reg_file), size);
 } END_TEST
 
+/* Test fsize with a character device. */
 START_TEST(test_fsize_char_file) {
     ck_assert_int_eq(fsize(char_file), -1);
 } END_TEST
 
+/* Test fsize with a directory. */
 START_TEST(test_fsize_dir_file) {
     ck_assert_int_eq(fsize(dir_file), -1);
 } END_TEST
@@ -64,11 +67,11 @@ START_TEST(test_fsize_dir_file) {
 // fskip
 
 void setup_fskip() {
+    // Create regular file of MAX_SIZE
     ck_assert(reg_file = tmpfile());
     ck_assert_int_eq(fzero(reg_file, MAX_SIZE), MAX_SIZE);
     // Return to start of file
     ck_assert_int_eq(fseek(reg_file, 0, SEEK_SET), 0);
-    ck_assert_int_eq(ftell(reg_file), 0);
     
     ck_assert(char_file = fopen("/dev/zero", "rb"));
 }
@@ -78,18 +81,21 @@ void teardown_fskip() {
     fclose(char_file);
 }
 
+/* Test fskip with various counts. */
 START_TEST(test_fskip) {
     size_t n = sizes[_i];
     ck_assert_int_eq(fskip(reg_file, n), n);
     ck_assert_int_eq(ftell(reg_file), n);
 } END_TEST
 
+/* Test fskip with various counts on a character device. */
 START_TEST(test_fskip_char) {
     size_t n = sizes[_i];
     // No real way to check position so we can't properly test this
     ck_assert_int_eq(fskip(char_file, n), n);
 } END_TEST
 
+/* Test fskip with various counts larger than the size of the file. */
 START_TEST(test_fskip_eof) {
     size_t n = sizes[_i];
     ck_assert_int_eq(fskip(reg_file, MAX_SIZE + n), MAX_SIZE);

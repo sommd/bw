86e7b7a6c396947fb69b1bd82409e7803b53c56d
sommd <d.j.s.1050@gmail.com>
2018-04-18 01:26:40 +1000

    Remove test_ prefix from check_error/assert_file_bytes

--- test/test.c
+++ test/test.c
@@ -6,11 +6,11 @@
 #include <check.h>
 #include "utils.h"
 
-void _test_check_error(const char *file, int line) {
+void _check_error(const char *file, int line) {
     _ck_assert_failed(file, line, "Failed", strerror(errno), NULL);
 }
 
-void test_assert_file_bytes(FILE *f, size_t n, byte b) {
+void assert_file_bytes(FILE *f, size_t n, byte b) {
     byte buf[BUF_SIZE];
     
     // Read up to n bytes or eof
@@ -19,7 +19,7 @@ void test_assert_file_bytes(FILE *f, size_t n, byte b) {
         size_t read = fread(buf, sizeof(byte), MIN(BUF_SIZE, n - total), f);
         
         // Check for read error
-        test_check_error(!ferror(f));
+        check_error(!ferror(f));
         
         // Assert all bytes zero
         for (size_t i = 0; i < read; i++) {
@@ -39,7 +39,7 @@ void write_junk(FILE *f, size_t n) {
     while (n > 0) {
         size_t to_write = MIN(BUF_SIZE, n);
         size_t written = fwrite(junk, sizeof(byte), to_write, f);
-        test_check_error(written == to_write);
+        check_error(written == to_write);
         
         n -= written;
     }
--- test/test.h
+++ test/test.h
@@ -4,13 +4,13 @@
 #include "utils.h"
 
 /* Abort with error message from errno if check is false. */
-#define test_check_error(check) if (!(check)) _test_check_error(__FILE__, __LINE__)
+#define check_error(check) if (!(check)) _check_error(__FILE__, __LINE__)
 
 /* Implementation of test_check_errno. */
-void _test_check_error(const char *file, int line);
+void _check_error(const char *file, int line);
 
 /* Assert that the next n bytes of f can be read and are all b. */
-void test_assert_file_bytes(FILE *f, size_t n, byte b);
+void assert_file_bytes(FILE *f, size_t n, byte b);
 
 /* Write n bytes of junk data to f. */
 void write_junk(FILE *f, size_t n);
